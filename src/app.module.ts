b916908f3609d371029d91dd09c36f257edb16b3
Dean Tawonezvi <deantawonezvi1@gmail.com>
2021-10-12 15:03:42 +0200

    Adds initial people module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
+import { PeopleModule } from './people/people.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot({})],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    PeopleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
